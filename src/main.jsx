c81d869bafb6e251016d32cc1cc91018f17c3e3f
Md Nazmul Hossen <nazmulhossen.hstu.21@gmail.com>
2025-04-13 21:22:18 +0600

    initialize react router

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const router = createBrowserRouter([
+  { path: "/", element: <h1>Welcome to react router</h1> },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    {/* <App /> */}
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
+);
